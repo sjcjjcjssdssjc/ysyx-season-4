287a131f6e2efc03a5cc1320bd7ade7c1e48673f
sjcjjcjssdssjc <1339590461@qq.com>
2022-02-22 22:47:27 -0800

    switch on NVboard

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -1,10 +1,43 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <assert.h>
+#include <nvboard.h>
 #include "Vtop.h"
 #include "verilated.h"
 
-int main(int argc, char** argv, char** env) {
+static TOP_NAME dut;
+
+void nvboard_bind_all_pins(Vtop* top);
+
+static void single_cycle() {
+  dut.a = rand() & 1;
+  dut.b = rand() & 1; 
+  dut.eval();
+  printf("a = %d, b = %d, f = %d\n", dut.a, dut.b, dut.f);
+  assert(dut.f == dut.a ^ dut.b);
+}
+
+
+/*
+static void reset(int n) {
+  dut.rst = 1;
+  while (n -- > 0) single_cycle();
+  dut.rst = 0;
+}
+*/
+
+int main() { // int argc, char** argv
+
+  nvboard_bind_all_pins(&dut);
+  nvboard_init();
+
+  //reset(10);
+
+  while(1) {
+    nvboard_update();
+    single_cycle();
+  }
+  /*
   VerilatedContext* contextp = new VerilatedContext;
   contextp->commandArgs(argc, argv);
   Vtop* top = new Vtop{contextp};
@@ -19,5 +52,6 @@ int main(int argc, char** argv, char** env) {
   }
   delete top;
   delete contextp;
+  */
   return 0;
 }
