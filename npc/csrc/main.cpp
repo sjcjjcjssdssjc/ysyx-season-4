6a011a7aae9e97e7796c8c1c29afd7d9275a85d1
sjcjjcjssdssjc <1339590461@qq.com>
2022-02-23 21:07:28 -0800

    day3 start

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -1,14 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <assert.h>
-//#include <nvboard.h>
+#include <nvboard.h>
 #include <memory>
 #include "Vtop.h"
 #include "verilated.h"
 #include "verilated_vcd_c.h"
-
 const std::unique_ptr<VerilatedContext> contextp{new VerilatedContext};
-void nvboard_bind_all_pins(Vtop* top);
+void nvboard_bind_all_pins(Vtop* dut);
 // Legacy function required only so linking works on Cygwin and MSVC++
 double sc_time_stamp() { return 0; }
 
@@ -16,9 +15,10 @@ static void single_cycle(Vtop * dut) {
   contextp->timeInc(1);
   // Toggle a fast (time/2 period) clock
   dut->clk = !dut->clk;
-  dut->a = rand() & 1;
-  dut->b = rand() & 1; 
+  dut->a = dut->rst ? 0 :rand() & 1;
+  dut->b = dut->rst ? 0 :rand() & 1; 
   dut->eval();
+  ///tfp->dump(contextp->time());
   VL_PRINTF("a = %x, b = %x, f = %x\n", dut->a, dut->b, dut->f);  
   //printf("a = %d, b = %d, f = %d\n", dut->a, dut->b, dut->f);
   assert(dut->f == dut->a ^ dut->b);
@@ -26,7 +26,12 @@ static void single_cycle(Vtop * dut) {
 
 static void reset(Vtop * dut,int n) {
   dut->rst = 1;
-  while (n -- > 0) single_cycle(dut);
+  while (n -- > 0) {
+    contextp->timeInc(1); 
+    dut->clk = !dut->clk;
+    dut->eval();
+    ///tfp->dump(contextp->time());
+  }
   dut->rst = 0;
 }
 
@@ -49,19 +54,21 @@ int main(int argc, char** argv, char** env) {
   // Pass arguments so Verilated code can see them, e.g. $value$plusargs
   // This needs to be called before you create any model
   contextp->commandArgs(argc, argv);
-
+  VerilatedVcdC* tfp = new VerilatedVcdC;
   Vtop * dut{new Vtop{contextp.get(), "TOP"}};
+  dut->trace(tfp, 99);
+  tfp->open("./obj_dir/wave.vcd");
+  Verilated::traceEverOn(true);
   dut->clk = 0;
   reset(dut,10);
-
   int sim_time = 50;
-  //nvboard_bind_all_pins(dut);
-  //nvboard_init();
-  while(contextp->time() < sim_time && !contextp->gotFinish()) {
-    //nvboard_update();
+  nvboard_bind_all_pins(dut);
+  nvboard_init();
+  while(!contextp->gotFinish()) {//contextp->time() < sim_time && 
+    nvboard_update();
     single_cycle(dut);
   }
-  //nvboard_quit();
+  nvboard_quit();
   dut->final();
   return 0;
 }
--- npc/vsrc/top.v
+++ npc/vsrc/top.v
@@ -3,22 +3,17 @@ module top(
   input rst,
   input a,
   input b,
-  output f
+  output wire f
 );
   assign f = a ^ b;
-  sub sub (/*AUTOINST*/
-      // Inputs
-      .clk                    (clk),
-      .reset_l                (rst));
-  // Print some stuff as an example
-   initial begin
+  initial begin
       if ($test$plusargs("trace") != 0) begin
          $display("[%0t] Tracing to logs/vlt_dump.vcd...\n", $time);
          $dumpfile("logs/vlt_dump.vcd");
          $dumpvars();
-      end else $display("[%0t] ???\n", $time);
+      end 
+      $display("%d %d",clk,rst);
       $display("[%0t] Model running...\n", $time);
-      $display("%d %d\n",clk,rst);
    end
 endmodule
 
