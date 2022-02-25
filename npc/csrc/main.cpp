872fb771cc4f04db5ece3131dc9c84f5b7391476
sjcjjcjssdssjc <1339590461@qq.com>
2022-02-24 17:51:26 -0800

    day4

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -19,8 +19,8 @@ int main(int argc, char** argv, char** env) {
   tfp->open("./build/obj_dir/wave.vcd");
 
   int sim_time = 50,n=10;//n to reset
-  nvboard_bind_all_pins(dut);
-  nvboard_init();
+  //nvboard_bind_all_pins(dut);
+  //nvboard_init();
 
   dut->rst = 1;
   dut->clk = 0;
@@ -30,25 +30,25 @@ int main(int argc, char** argv, char** env) {
     dut->clk = !dut->clk; 
     dut->eval();
     
-    //tfp->dump(contextp->time());
+    tfp->dump(contextp->time());
   }
 
   //contextp->time() < 50 && 
-  while(!contextp->gotFinish()) {//contextp->time() < sim_time && 
-    nvboard_update();
+  while(contextp->time() < sim_time && !contextp->gotFinish()) {//contextp->time() < sim_time && 
+    //nvboard_update();
     contextp->timeInc(1);
     dut->clk = !dut->clk;
-    //dut->a = rand() & 1;
-    //dut->b = rand() & 1; 
+    dut->d = dut->d + 1;
+    dut->load = rand() & 1; 
     dut->eval();
 
     dut->rst = 0;//to fix the sample bug
-    //tfp->dump(contextp->time());
+    tfp->dump(contextp->time());
     //printf("a = %d, b = %d, f = %d\n", dut->a, dut->b, dut->f);
     ///assert(dut->f == dut->a ^ dut->b);
   }
   tfp->close();
-  nvboard_quit();
+  //nvboard_quit();
   dut->final();
   return 0;
 }
--- npc/vsrc/top.v
+++ npc/vsrc/top.v
@@ -1,20 +1,12 @@
-module top(
-  input clk,
-  input rst,
-  input[1:0] X0,
-  input[1:0] X1,
-  input[1:0] X2,
-  input[1:0] X3,
-  input[1:0] Y,
-  output reg[1:0] F
-);
-  always @(X0 or X1 or X2 or X3) begin
-    case(Y)
-      2'b00: F = X0;
-      2'b01: F = X1;
-      2'b10: F = X2;
-      2'b11: F = X3;
-    endcase
-  end
+module top(load,clk,rst,d,q);
+  input  load,rst,clk;
+  input  [3:0] d;
+  output reg [3:0] q;
+
+  always @(posedge clk)
+    if (rst==1)
+        q <= 0;
+    else if (load == 1)
+        q <= d;
 endmodule
 
