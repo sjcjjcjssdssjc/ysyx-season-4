837959a3bab2fe78f6329ee63dfc7df2875f08ef
tracer-ysyx2204 <tracer@ysyx.org>
2022-03-01 18:31:23 -0800

    >  compile NEMU ysyx_22040127 shijiacheng Linux ubuntu 5.13.0-30-generic #33~20.04.1-Ubuntu SMP Mon Feb 7 14:25:10 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  18:31:23 up 32 min,  1 user,  load average: 0.82, 0.61, 0.52

--- nemu/src/monitor/sdb/sdb.c
+++ nemu/src/monitor/sdb/sdb.c
@@ -37,6 +37,10 @@ static int cmd_q(char *args) {
   cpu_exec(0);
   exit(0);
 }
+static int cmd_si(char *args) {
+  cpu_exec(0);
+  exit(0);
+}
 
 static int cmd_help(char *args);
 
@@ -48,7 +52,7 @@ static struct {
   { "help", "Display informations about all supported commands", cmd_help },
   { "c", "Continue the execution of the program", cmd_c },
   { "q", "Exit NEMU", cmd_q },
-
+  { "si","single step the program", cmd_si}
   /* TODO: Add more commands */
 
 };
