dc0c6a5aaa35d1097b13f61679e5058a445c98e3
tracer-ysyx2204 <tracer@ysyx.org>
2022-03-06 02:18:23 -0800

    >  compile NEMU ysyx_22040127 shijiacheng Linux ubuntu 5.13.0-30-generic #33~20.04.1-Ubuntu SMP Mon Feb 7 14:25:10 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  02:18:23 up  6:04,  1 user,  load average: 0.28, 0.28, 0.28

--- nemu/src/isa/riscv64/inst.c
+++ nemu/src/isa/riscv64/inst.c
@@ -9,7 +9,7 @@
 
 enum {
   TYPE_I, TYPE_U, TYPE_S,
-  TYPE_N, // none
+  TYPE_N, TYPE_J, TYPE_R // none
 };
 
 #define src1R(n) do { *src1 = R(n); } while (0)
@@ -22,20 +22,30 @@ enum {
 static word_t immI(uint32_t i) { return SEXT(BITS(i, 31, 20), 12); }
 static word_t immU(uint32_t i) { return SEXT(BITS(i, 31, 12), 20) << 12; }
 static word_t immS(uint32_t i) { return (SEXT(BITS(i, 31, 25), 7) << 5) | BITS(i, 11, 7); }
-
+static word_t immJ(uint32_t i) { return (SEXT(BITS(i, 20, 20), 1) << 19) | BITS(i, 10, 1) << 9 | BITS(i, 11, 11) << 8 | BITS(i, 19, 12); }
 static void decode_operand(Decode *s, word_t *dest, word_t *src1, word_t *src2, int type) {
   uint32_t i = s->isa.inst.val;
   int rd  = BITS(i, 11, 7);
   int rs1 = BITS(i, 19, 15);
   int rs2 = BITS(i, 24, 20);
-  destR(rd);
+  destR(rd);//dest register is always[11:7]
   switch (type) {
     case TYPE_I: src1R(rs1);     src2I(immI(i)); break;
     case TYPE_U: src1I(immU(i)); break;
     case TYPE_S: destI(immS(i)); src1R(rs1); src2R(rs2); break;
+    case TYPE_J: src1I(immJ(i)); break;
+    case TYPE_R: src1R(rs1); src2R(rs2); break;
   }
 }
-
+static void jal_op(word_t dest, word_t src1, Decode *s){
+  R(dest) = s->snpc; 
+  s->dnpc += src1;
+}
+static void jalr_op(word_t dest, word_t src1, word_t src2, Decode *s){
+  word_t t = s->snpc; 
+  s->dnpc = (src1 + src2) & (~1);
+  R(dest) = t;
+}
 static int decode_exec(Decode *s) {
   word_t dest = 0, src1 = 0, src2 = 0;
   s->dnpc = s->snpc;//dynamic next pc <- static next pc
@@ -47,13 +57,16 @@ static int decode_exec(Decode *s) {
 }
 
   INSTPAT_START();
-
+  //                                                                      //R(rs1)
   INSTPAT("??????? ????? ????? ??? ????? 00101 11", auipc  , U, R(dest) = src1 + s->pc);
-        // IMM[11:0]----           rd[11:7] opcode[6:0]        dest(addr)
+  INSTPAT("0000000 ????? ????? 000 ????? 01100 11", add    , R, R(dest) = src1 + src2);
+        // IMM[31:20]--- rs[19:15] rd[11:7] opcode[6:0]        dest(addr)
   INSTPAT("??????? ????? ????? 011 ????? 00000 11", ld     , I, R(dest) = Mr(src1 + src2, 8));
-  INSTPAT("??????? ????? ????? ??? ????? 00100 11", li     , I, R(dest) = Mr(src2 , 8));//if collide then edit
+  INSTPAT("??????? ????? ????? 000 ????? 00100 11", addi   , I, R(dest) = src1 + src2);//(li/mv?)
+  INSTPAT("??????? ????? ????? 000 ????? 11001 11", jalr   , I, jalr_op(dest, src1, src2, s));//(ret)
   INSTPAT("??????? ????? ????? 011 ????? 01000 11", sd     , S, Mw(src1 + dest, 8, src2));
 
+  INSTPAT("??????? ????? ????? ??? ????? 11011 11", jal    , J, jal_op(dest, src1, s));
   INSTPAT("0000000 00001 00000 000 00000 11100 11", ebreak , N, NEMUTRAP(s->pc, R(10))); // R(10) is $a0
   INSTPAT("??????? ????? ????? ??? ????? ????? ??", inv    , N, INV(s->pc));
   INSTPAT_END();
