34dc59458c3d307e6a3ffe3b8b51af09126d0e70
tracer-ysyx2204 <tracer@ysyx.org>
2022-03-08 07:29:44 -0800

    >  compile NEMU ysyx_22040127 shijiacheng Linux ubuntu 5.13.0-30-generic #33~20.04.1-Ubuntu SMP Mon Feb 7 14:25:10 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  07:29:44 up  5:15,  1 user,  load average: 0.78, 0.65, 0.55

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -16,13 +16,14 @@ static uint64_t g_timer = 0; // unit: us
 static bool g_print_step = false;
 
 void device_update();
-
+void seek_changes();
 static void trace_and_difftest(Decode *_this, vaddr_t dnpc) {
 #ifdef CONFIG_ITRACE_COND
   if (ITRACE_COND) { log_write("%s\n", _this->logbuf); }
 #endif
   if (g_print_step) { IFDEF(CONFIG_ITRACE, puts(_this->logbuf)); }
   IFDEF(CONFIG_DIFFTEST, difftest_step(_this->pc, dnpc));
+  seek_changes();
   //IFDEF(CONFIG_WATCHPOINT, func);
 }
 
--- nemu/src/monitor/sdb/watchpoint.c
+++ nemu/src/monitor/sdb/watchpoint.c
@@ -61,7 +61,7 @@ void seek_changes(){
   WP * now;
   for(now = head; now != NULL; now = now -> next){
     bool sanity;
-    word_t val = expr(now -> expr, &sanity) ;
+    word_t val = expr(now -> expr, &sanity);//expr has no ""
     if(!sanity){
       printf("the watchpoint is insane\n");
     }
@@ -70,6 +70,7 @@ void seek_changes(){
       printf("watchpoint %s changed from %ld(%lx)to %ld(%lx)\n",now -> expr, 
       now -> val, now -> val, val, val);
       now -> val = val;
+
     }
   }
 }
