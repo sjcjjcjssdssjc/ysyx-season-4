3d223b62e715cb40345bb6c0e78c3bf9878776aa
tracer-ysyx2204 <tracer@ysyx.org>
2022-03-10 06:18:29 -0800

    >  compile NEMU ysyx_22040127 shijiacheng Linux ubuntu 5.13.0-35-generic #40~20.04.1-Ubuntu SMP Mon Mar 7 09:18:32 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  06:18:29 up  5:29,  1 user,  load average: 0.27, 0.43, 0.44

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -23,6 +23,7 @@ void seek_changes();
 static void print_surrounding_inst(){
   
   int i = 0;
+  printf("%d\n",overburden);
   if(overburden)i = (iring_tail) % IRINGBUF_SIZE;
   for(; i != iring_tail; i = (i + 1) % IRINGBUF_SIZE){
     if(i != iring_tail)printf("    %s\n",iringbuf[i]);
