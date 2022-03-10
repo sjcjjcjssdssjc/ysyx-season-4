ba41ef219e47e1cef6d4eee3e6820f3503cc7ff4
tracer-ysyx2204 <tracer@ysyx.org>
2022-03-09 22:58:51 -0800

    >  compile NEMU ysyx_22040127 shijiacheng Linux ubuntu 5.13.0-35-generic #40~20.04.1-Ubuntu SMP Mon Mar 7 09:18:32 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  22:58:51 up 54 min,  1 user,  load average: 0.25, 0.43, 0.49

--- abstract-machine/klib/src/string.c
+++ abstract-machine/klib/src/string.c
@@ -94,7 +94,7 @@ void *memcpy(void *dest, const void *src, size_t n) {
 int memcmp(const void *s1, const void *s2, size_t n) {
   const unsigned char *su1, *su2;
 	int res = 0;
-	for (su1 = s1, su2 = s2; 0 < n; ++su1, ++su2, n--)
+	for (su1 = s1, su2 = s2; n > 0; ++su1, ++su2, n--)
 		if ((res = *su1 - *su2) != 0)
 			break;
 	return res;
