9ba75e58843a0680523d439128df6809bc80cafa
tracer-ysyx2204 <tracer@ysyx.org>
2022-03-11 21:21:16 -0800

    >  compile NEMU ysyx_22040127 shijiacheng Linux ubuntu 5.13.0-35-generic #40~20.04.1-Ubuntu SMP Mon Mar 7 09:18:32 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  21:21:16 up  5:01,  1 user,  load average: 0.25, 0.44, 0.60

--- nemu/src/cpu/difftest/dut.c
+++ nemu/src/cpu/difftest/dut.c
@@ -88,6 +88,7 @@ void difftest_step(vaddr_t pc, vaddr_t npc) {
   CPU_state ref_r;
 
   if (skip_dut_nr_inst > 0) {
+    printf("qemu skipped\n");
     ref_difftest_regcpy(&ref_r, DIFFTEST_TO_DUT);
     if (ref_r.pc == npc) {
       skip_dut_nr_inst = 0;
