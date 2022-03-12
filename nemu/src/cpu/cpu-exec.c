62c27c1079cbf526880041c6f1dcfd330da764be
tracer-ysyx2204 <tracer@ysyx.org>
2022-03-11 23:00:04 -0800

    >  compile NEMU ysyx_22040127 shijiacheng Linux ubuntu 5.13.0-35-generic #40~20.04.1-Ubuntu SMP Mon Mar 7 09:18:32 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  23:00:04 up  6:39,  1 user,  load average: 0.59, 0.48, 0.44

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -27,7 +27,10 @@ bool ftrace = 0;
 void device_update();
 void seek_changes();
 void parse_elf(const char *elf_file){
-  if(elf_file == NULL)return;
+  if(elf_file == NULL){
+    printf("Elf file missing\n");
+    return;
+  }
   ftrace = 1;//give elf, so we trace
   FILE *fp;
   fp = fopen(elf_file, "rb");
