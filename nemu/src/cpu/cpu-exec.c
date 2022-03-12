21221662ac2d469fcec29a1694998884187c1ff9
tracer-ysyx2204 <tracer@ysyx.org>
2022-03-12 07:54:26 -0800

    >  compile NEMU ysyx_22040127 shijiacheng Linux ubuntu 5.13.0-35-generic #40~20.04.1-Ubuntu SMP Mon Mar 7 09:18:32 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  07:54:26 up  9:49,  1 user,  load average: 0.64, 0.62, 0.57

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -78,7 +78,7 @@ void parse_elf(const char *elf_file){
       ret = fread(symtab, shdr[i].sh_size, 1, fp);
       printf("%ld %ld\n",shdr[i].sh_size , sizeof(Elf64_Sym));
       for(int j = 0;j < shdr[i].sh_size / sizeof(Elf64_Sym); j++){
-        printf("%d %s\n",symtab[j].st_name, strtab + symtab[j].st_name);
+        printf("%d:%d %s\n",j, symtab[j].st_name, strtab + symtab[j].st_name);
       }
       
     }
