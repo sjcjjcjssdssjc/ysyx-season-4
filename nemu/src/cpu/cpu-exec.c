7c3acfd77808fa1a4deccdb7833d2325deae588a
tracer-ysyx2204 <tracer@ysyx.org>
2022-03-12 22:38:42 -0800

    >  compile NEMU ysyx_22040127 shijiacheng Linux ubuntu 5.13.0-35-generic #40~20.04.1-Ubuntu SMP Mon Mar 7 09:18:32 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  22:38:42 up 12:21,  1 user,  load average: 0.91, 0.51, 0.47

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -124,21 +124,22 @@ static void trace_and_difftest(Decode *_this, vaddr_t dnpc) {
 
 static void exec_once(Decode *s, vaddr_t pc) {
 #ifdef CONFIG_FTRACE
-  vaddr_t pre = s->pc;
+  //vaddr_t pre = s->pc;//1
 #endif
   s->pc = pc;
   s->snpc = pc;
   isa_exec_once(s);
   cpu.pc = s->dnpc;
-
+  printf("%ld %ld\n",s->snpc,cpu.pc);
 #ifdef CONFIG_FTRACE
-  int ret = 0;
+  //int ret = 0;//2
   //printf("%x\n",(s)->isa.inst.val);
-  if(((s)->isa.inst.val) == 0x8067)ret = 1;
+  //if(((s)->isa.inst.val) == 0x8067)ret = 1;//3
   if(symtab){
     for(int i = 0;i < symtab_len; i++){
       //printf("%lx:%d %s\n",symtab[j].st_value, symtab[j].st_name, symstrtab + symtab[j].st_name);
      
+      /*
       if(symtab[i].st_value == cpu.pc && pre <= 0x80800000 && !ret){
           //printf("%d\n",stksiz);
           for(int i = 0;i < stksiz; i++)printf(" ");
@@ -150,6 +151,7 @@ static void exec_once(Decode *s, vaddr_t pc) {
         for(int i = 0;i < stksiz; i++)printf(" ");
         printf("%lx: ret [%s]\n",pre, symstrtab + symtab[i].st_name);
       }
+      */
     }
     
   }
