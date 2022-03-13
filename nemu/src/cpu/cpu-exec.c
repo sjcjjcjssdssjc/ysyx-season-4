d8812fe7c5bcfc9d412e35488c57cc69155a4e8e
tracer-ysyx2204 <tracer@ysyx.org>
2022-03-12 21:26:36 -0800

    >  compile NEMU ysyx_22040127 shijiacheng Linux ubuntu 5.13.0-35-generic #40~20.04.1-Ubuntu SMP Mon Mar 7 09:18:32 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  21:26:36 up 11:08,  1 user,  load average: 0.19, 0.30, 0.66

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -120,6 +120,7 @@ static void trace_and_difftest(Decode *_this, vaddr_t dnpc) {
 }
 
 static void exec_once(Decode *s, vaddr_t pc) {
+  printf("%lx\n",pc);
   s->pc = pc;
   s->snpc = pc;
   isa_exec_once(s);
