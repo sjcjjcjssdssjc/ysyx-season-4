efa3ae8afe2e37b65542aec5e6df12e198c68c78
tracer-ysyx2204 <tracer@ysyx.org>
2022-03-15 07:12:27 -0700

    >  compile NEMU ysyx_22040127 shijiacheng Linux ubuntu 5.13.0-35-generic #40~20.04.1-Ubuntu SMP Mon Mar 7 09:18:32 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  07:12:27 up 12:47,  1 user,  load average: 0.30, 0.41, 0.40

--- abstract-machine/am/src/platform/nemu/include/nemu.h
+++ abstract-machine/am/src/platform/nemu/include/nemu.h
@@ -25,7 +25,7 @@
 #define MMIO_BASE 0xa0000000
 
 #define SERIAL_PORT     (DEVICE_BASE + 0x00003f8)
-#define KBD_ADDR        (DEVICE_BASE + 0x0000060)
+#define KBD_ADDR        (DEVICE_BASE + 0x0000060)//keyboard
 #define RTC_ADDR        (DEVICE_BASE + 0x0000048)
 #define VGACTL_ADDR     (DEVICE_BASE + 0x0000100)
 #define AUDIO_ADDR      (DEVICE_BASE + 0x0000200)
