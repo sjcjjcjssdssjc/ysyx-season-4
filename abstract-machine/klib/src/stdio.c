642c7fab6d509f72ae077aa19928669c93577538
tracer-ysyx2204 <tracer@ysyx.org>
2022-03-16 06:49:20 -0700

    >  compile NEMU ysyx_22040127 shijiacheng Linux ubuntu 5.13.0-35-generic #40~20.04.1-Ubuntu SMP Mon Mar 7 09:18:32 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  06:49:20 up 14:26,  1 user,  load average: 2.22, 1.54, 0.88

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -4,19 +4,44 @@
 #include <stdarg.h>
 
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
-char out[5000];//to be changed
 int printf(const char *fmt, ...) {
   
-  int ret;
   va_list ap;
   va_start(ap, fmt);
-  ret = vsprintf(out, fmt, ap);
+  int now;
+  for(now = 0;*fmt != 0;fmt++){
+    if(*fmt != '%'){
+      putch(*fmt);
+      now++;
+    }
+    else{
+      fmt++;
+      if(*fmt == 'd'){
+        int num = va_arg(ap, int);
+        int rev = 0,len = 0;
+        while(num){
+          rev *= 10;
+          rev += num % 10;
+          num /= 10;
+          len++;
+        }
+        while(len--){
+          putch(rev % 10 + '0');
+          rev /= 10;
+        }
+      }
+      else if(*fmt == 's'){//s
+        char *tmp = va_arg(ap, char *);
+        int l = strlen(tmp);
+        while(l--){
+          putch(*tmp++);
+          now++;
+        }
+      }
+    }
+  }
   va_end(ap);
-  for(int i = 0; i < ret; i++)
-    putch(out[i]);
-  return ret;
-  
- return 0;
+  return now;//return the length of the string
 }
 
 int vsprintf(char *out, const char *fmt, va_list ap) {
