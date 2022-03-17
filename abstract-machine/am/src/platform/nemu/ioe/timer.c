019187a4b04cba4f09390194750f863e0c3682c8
tracer-ysyx2204 <tracer@ysyx.org>
2022-03-17 05:32:31 -0700

    >  run NEMU ysyx_22040127 shijiacheng Linux ubuntu 5.13.0-35-generic #40~20.04.1-Ubuntu SMP Mon Mar 7 09:18:32 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  05:32:31 up 21:41,  1 user,  load average: 0.24, 0.27, 0.35

--- abstract-machine/am/src/platform/nemu/ioe/timer.c
+++ abstract-machine/am/src/platform/nemu/ioe/timer.c
@@ -4,7 +4,7 @@
 void __am_timer_init() {
 }
 void __am_timer_uptime(AM_TIMER_UPTIME_T *uptime) {
-  uptime->us+= 10;//skip first
+  uptime->us += 10;//skip first
 }
 
 void __am_timer_rtc(AM_TIMER_RTC_T *rtc) {
