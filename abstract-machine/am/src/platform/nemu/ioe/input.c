f9427397493e035c5dfd613e6bc90e690caacc6c
tracer-ysyx2204 <tracer@ysyx.org>
2022-03-18 00:33:50 -0700

    >  run NEMU ysyx_22040127 shijiacheng Linux ubuntu 5.13.0-35-generic #40~20.04.1-Ubuntu SMP Mon Mar 7 09:18:32 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  00:33:50 up  1:41,  1 user,  load average: 0.90, 0.60, 0.50

--- abstract-machine/am/src/platform/nemu/ioe/input.c
+++ abstract-machine/am/src/platform/nemu/ioe/input.c
@@ -5,7 +5,7 @@
 
 void __am_input_keybrd(AM_INPUT_KEYBRD_T *kbd) {
   uint32_t am_scancode = inl(KBD_ADDR);
-  printf("%d\n",am_scancode);
+  if(am_scancode)printf("%d\n",am_scancode);
   kbd->keydown = 0;
   kbd->keycode = AM_KEY_NONE;
 }
