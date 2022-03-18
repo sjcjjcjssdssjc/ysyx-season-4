6ef2b80f9cdb4b1b190f3d3c52e4f270cb302b6f
tracer-ysyx2204 <tracer@ysyx.org>
2022-03-18 01:04:21 -0700

    >  run NEMU ysyx_22040127 shijiacheng Linux ubuntu 5.13.0-35-generic #40~20.04.1-Ubuntu SMP Mon Mar 7 09:18:32 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  01:04:21 up  2:11,  1 user,  load average: 0.19, 0.29, 0.35

--- nemu/src/device/keyboard.c
+++ nemu/src/device/keyboard.c
@@ -72,8 +72,7 @@ static void i8042_data_io_handler(uint32_t offset, int len, bool is_write) {
   assert(!is_write);
   assert(offset == 0);
   i8042_data_port_base[0] = key_dequeue();
-  if(i8042_data_port_base[0])
-  printf("%x\n",i8042_data_port_base[0]);
+  if(i8042_data_port_base[0])printf("%x\n",i8042_data_port_base[0]);
 }
 
 void init_i8042() {
