47cb8e7718844ba4763edee06b7e01f253301d6b
tracer-ysyx2204 <tracer@ysyx.org>
2022-03-18 01:02:17 -0700

    >  run NEMU ysyx_22040127 shijiacheng Linux ubuntu 5.13.0-35-generic #40~20.04.1-Ubuntu SMP Mon Mar 7 09:18:32 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  01:02:17 up  2:09,  1 user,  load average: 0.18, 0.28, 0.36

--- nemu/src/device/keyboard.c
+++ nemu/src/device/keyboard.c
@@ -72,10 +72,10 @@ static void i8042_data_io_handler(uint32_t offset, int len, bool is_write) {
   assert(!is_write);
   assert(offset == 0);
   i8042_data_port_base[0] = key_dequeue();
+  printf("%x\n",i8042_data_port_base[0]);
 }
 
 void init_i8042() {
-  printf("inited\n");
   i8042_data_port_base = (uint32_t *)new_space(4);
   i8042_data_port_base[0] = _KEY_NONE;
 #ifdef CONFIG_HAS_PORT_IO
