8a2255c6ee5b25139398e9ed380e9ed2f45ec46e
tracer-ysyx2204 <tracer@ysyx.org>
2022-03-18 04:46:27 -0700

    >  run NEMU ysyx_22040127 shijiacheng Linux ubuntu 5.13.0-35-generic #40~20.04.1-Ubuntu SMP Mon Mar 7 09:18:32 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  04:46:27 up  5:04,  1 user,  load average: 0.94, 0.70, 0.54

--- abstract-machine/am/src/platform/nemu/ioe/gpu.c
+++ abstract-machine/am/src/platform/nemu/ioe/gpu.c
@@ -24,19 +24,20 @@ void __am_gpu_config(AM_GPU_CONFIG_T *cfg) {
 
 void __am_gpu_fbdraw(AM_GPU_FBDRAW_T *ctl) {
   if (ctl->sync) {
-    /*
+    
     int i;
-    int x = ctl -> x;
-    int y = ctl -> y;
-    int w = ctl -> w;//io_read(AM_GPU_CONFIG).width;  // TODO: get the correct width
-    int h = ctl -> h;//io_read(AM_GPU_CONFIG).height;  // TODO: get the correct height
+    int x = io_read(AM_GPU_FBDRAW).x;//ctl -> x;
+    int y = io_read(AM_GPU_FBDRAW).y;
+    int w = io_read(AM_GPU_FBDRAW).w;//ctl -> w;
+    int h = io_read(AM_GPU_FBDRAW).h;//ctl -> h;
+    uint32_t * pixels = io_read(AM_GPU_FBDRAW).pixels;
     //printf("w is %d h is %d\n",w,h);
     uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;//frame_buffer
     printf("%d %d %d %d\n",x,y,w,h);
     for (i = x * w + y; i < x * w + y + w*h; i ++) {
-      fb[i] = ((uint32_t *)(ctl -> pixels))[i - x * w - y];
+      fb[i] = pixels[i - x * w - y];
     }
-    */
+    
     outl(SYNC_ADDR, 1);
   }
 }
