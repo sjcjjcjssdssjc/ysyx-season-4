324bcc3fa33683f1ca55ef51a132e6191f7fe634
tracer-ysyx2204 <tracer@ysyx.org>
2022-03-17 23:54:34 -0700

    >  run NEMU ysyx_22040127 shijiacheng Linux ubuntu 5.13.0-35-generic #40~20.04.1-Ubuntu SMP Mon Mar 7 09:18:32 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  23:54:34 up  1:01,  1 user,  load average: 0.92, 0.78, 0.75

--- nemu/src/device/io/map.c
+++ nemu/src/device/io/map.c
@@ -40,7 +40,9 @@ void init_map() {
 word_t map_read(paddr_t addr, int len, IOMap *map) {
   assert(len >= 1 && len <= 8);
   check_bound(map, addr);
-  //printf("read device %s\n",map->name);
+  #ifdef CONFIG_DTRACE
+  printf("read device %s\n",map->name);
+  #endif
   paddr_t offset = addr - map->low;
   invoke_callback(map->callback, offset, len, false); // prepare data to read(lazy allocation?)
   word_t ret = host_read(map->space + offset, len);
@@ -50,7 +52,9 @@ word_t map_read(paddr_t addr, int len, IOMap *map) {
 void map_write(paddr_t addr, int len, word_t data, IOMap *map) {
   assert(len >= 1 && len <= 8);
   check_bound(map, addr);
-  //printf("write device %s\n",map->name);
+  #ifdef CONFIG_DTRACE
+  printf("write device %s\n",map->name);
+  #endif
   paddr_t offset = addr - map->low;
   host_write(map->space + offset, len, data);
   invoke_callback(map->callback, offset, len, true);
