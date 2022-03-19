91f6545623ca08ffd444bca08bebf7f8d054b398
tracer-ysyx2204 <tracer@ysyx.org>
2022-03-18 18:39:06 -0700

    >  run NEMU ysyx_22040127 shijiacheng Linux ubuntu 5.13.0-35-generic #40~20.04.1-Ubuntu SMP Mon Mar 7 09:18:32 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  18:39:06 up  9:41,  1 user,  load average: 0.49, 0.52, 0.55

--- nemu/src/cpu/difftest/dut.c
+++ nemu/src/cpu/difftest/dut.c
@@ -109,6 +109,7 @@ void difftest_step(vaddr_t pc, vaddr_t npc) {
   }
 
   ref_difftest_exec(1);
+  printf("ref exec 1\n");
   ref_difftest_regcpy(&ref_r, DIFFTEST_TO_DUT);//ref_r is ref(spike)
 
   checkregs(&ref_r, pc);
