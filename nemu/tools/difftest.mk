883d32662f782278dfd34ff0441d952f934432e8
tracer-ysyx2204 <tracer@ysyx.org>
2022-03-20 07:30:25 -0700

    >  run NEMU ysyx_22040127 shijiacheng Linux ubuntu 5.13.0-35-generic #40~20.04.1-Ubuntu SMP Mon Mar 7 09:18:32 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  07:30:25 up  5:21,  1 user,  load average: 1.73, 1.03, 0.63

--- nemu/tools/difftest.mk
+++ nemu/tools/difftest.mk
@@ -1,7 +1,8 @@
 ifdef CONFIG_DIFFTEST
 DIFF_REF_PATH = $(NEMU_HOME)/$(call remove_quote,$(CONFIG_DIFFTEST_REF_PATH))
-$(info ref path is $(CONFIG_DIFFTEST_REF_PATH))
+$(info ref path is $(CONFIG_DIFFTEST_REF_PATH))#"tools/spike-diff" in this case
 DIFF_REF_SO = $(DIFF_REF_PATH)/build/$(GUEST_ISA)-$(call remove_quote,$(CONFIG_DIFFTEST_REF_NAME))-so
+$(info ref so in $(DIFF_REF_SO))#"tools/spike-diff" in this case
 MKFLAGS = GUEST_ISA=$(GUEST_ISA) SHARE=1 ENGINE=interpreter
 ARGS_DIFF = --diff=$(DIFF_REF_SO)
 
