3775ede1f5f5ce752d8a8f20ac4deef329aee6d9
tracer-ysyx2204 <tracer@ysyx.org>
2022-03-25 04:55:11 -0700

    >  run NEMU ysyx_22040127 shijiacheng Linux ubuntu 5.13.0-35-generic #40~20.04.1-Ubuntu SMP Mon Mar 7 09:18:32 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  04:55:11 up 14:56,  1 user,  load average: 0.51, 0.34, 0.24

--- abstract-machine/am/src/riscv/nemu/cte.c
+++ abstract-machine/am/src/riscv/nemu/cte.c
@@ -9,7 +9,8 @@ Context* __am_irq_handle(Context *c) {//handler function(hui diao)
   if (user_handler) {
     Event ev = {0};
     switch (c->mcause) {
-      default: ev.event = EVENT_ERROR; break;
+      case(-1): ev.event = EVENT_YIELD; break;
+      default:  ev.event = EVENT_ERROR; break;
     }
 
     c = user_handler(ev, c);
