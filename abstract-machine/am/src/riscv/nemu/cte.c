13623a204a98b8035b740efaf35b1d83462e4482
tracer-ysyx2204 <tracer@ysyx.org>
2022-03-25 01:50:17 -0700

    >  run NEMU ysyx_22040127 shijiacheng Linux ubuntu 5.13.0-35-generic #40~20.04.1-Ubuntu SMP Mon Mar 7 09:18:32 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  01:50:17 up 12:57,  1 user,  load average: 0.89, 0.60, 0.50

--- abstract-machine/am/src/riscv/nemu/cte.c
+++ abstract-machine/am/src/riscv/nemu/cte.c
@@ -5,7 +5,7 @@
 static Context* (*user_handler)(Event, Context*) = NULL;
 
 Context* __am_irq_handle(Context *c) {
-  printf("handle %lx\n",c->mcause);
+  printf("handle %d\n",c->mcause);
   if (user_handler) {
     Event ev = {0};
     switch (c->mcause) {
