f180151d8d7ce402d20b60bd16d974b1d10cb4d6
tracer-ysyx2204 <tracer@ysyx.org>
2022-03-25 05:42:16 -0700

    >  run NEMU ysyx_22040127 shijiacheng Linux ubuntu 5.13.0-35-generic #40~20.04.1-Ubuntu SMP Mon Mar 7 09:18:32 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  05:42:16 up 15:43,  1 user,  load average: 0.44, 0.47, 0.40

--- abstract-machine/am/src/riscv/nemu/cte.c
+++ abstract-machine/am/src/riscv/nemu/cte.c
@@ -5,9 +5,7 @@ static Context* (*user_handler)(Event, Context*) = NULL;
 
 Context* __am_irq_handle(Context *c) {//handler function(hui diao)
 
-  #ifdef CONFIG_ETRACE
-  printf("handle mepc %lx mcause%lx mstatus%lx\n",c->mepc, c->mcause, c->mstatus);//no mtvec
-  #endif
+  //printf("handle mepc %lx mcause%lx mstatus%lx\n",c->mepc, c->mcause, c->mstatus);//no mtvec
   if (user_handler) {
     Event ev = {0};
     switch (c->mcause) {
--- nemu/src/isa/riscv64/system/intr.c
+++ nemu/src/isa/riscv64/system/intr.c
@@ -4,11 +4,14 @@ word_t isa_raise_intr(word_t NO, vaddr_t epc) {
   /* TODO: Trigger an interrupt/exception with ``NO''.
    * Then return the address of the interrupt/exception vector.
    */
+
   word_t mtvec;
   cpu.mepc   = epc;
   cpu.mcause = NO;
   mtvec      = cpu.mtvec;
-  
+  #ifdef CONFIG_ETRACE
+  printf("handle mepc %lx mcause %lx mstatus %lx\n",cpu.mepc, cpu.mcause, cpu.mstatus);//no mtvec
+  #endif
   //1:将当前PC值保存到mepc寄存器
   //2:在mcause寄存器中设置异常号
   return mtvec;//从mtvec寄存器中取出异常入口地址,跳转到异常入口地址(nemu dnpc = mtvec)
