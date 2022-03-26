ee91344a0407b9f068869f65fa4b26460fd168f5
tracer-ysyx2204 <tracer@ysyx.org>
2022-03-25 22:38:40 -0700

    >  run NEMU ysyx_22040127 shijiacheng Linux ubuntu 5.13.0-35-generic #40~20.04.1-Ubuntu SMP Mon Mar 7 09:18:32 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  22:38:40 up 17:47,  1 user,  load average: 1.01, 0.72, 0.50

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -9,8 +9,20 @@
 # define Elf_Ehdr Elf32_Ehdr
 # define Elf_Phdr Elf32_Phdr
 #endif
+size_t ramdisk_read(void *buf, size_t offset, size_t len);
+size_t ramdisk_write(const void *buf, size_t offset, size_t len);
+size_t get_ramdisk_size();
 
 static uintptr_t loader(PCB *pcb, const char *filename) {
+  size_t offset = 0;
+  Elf_Ehdr header; //elf header
+  offset += ramdisk_read(&header, offset, sizeof(header));
+  if(header.e_type == ET_EXEC  &&
+       header.e_ident[1] == 'E' &&
+       header.e_ident[2] == 'L' &&
+       header.e_ident[3] == 'F') {
+       printf("ok\n");
+  }
   return 0;
 }
 
