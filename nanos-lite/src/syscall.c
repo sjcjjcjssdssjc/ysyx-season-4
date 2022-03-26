e9794f32490daae3eb83c6fbdfafb42fd141a2ec
tracer-ysyx2204 <tracer@ysyx.org>
2022-03-26 04:41:39 -0700

    >  run NEMU ysyx_22040127 shijiacheng Linux ubuntu 5.13.0-35-generic #40~20.04.1-Ubuntu SMP Mon Mar 7 09:18:32 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  04:41:39 up 20:50,  1 user,  load average: 0.38, 0.31, 0.33

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -4,8 +4,8 @@ void do_syscall(Context *c) {
   printf("do syscall\n");
   uintptr_t a[4];
   a[0] = c->GPR1;
-
+  int b = 3;
   switch (a[0]) {
-    default: panic("Unhandled syscall ID = %x", a[0]);
+    default: panic("Unhandled syscall ID = %x %x", a[0],b);
   }
 }
