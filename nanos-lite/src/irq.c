761cfbb0eb1c8a5f146b3b1235177ac59522e042
tracer-ysyx2204 <tracer@ysyx.org>
2022-03-26 04:27:10 -0700

    >  run NEMU ysyx_22040127 shijiacheng Linux ubuntu 5.13.0-35-generic #40~20.04.1-Ubuntu SMP Mon Mar 7 09:18:32 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  04:27:10 up 20:36,  1 user,  load average: 1.11, 0.58, 0.39

--- nanos-lite/src/irq.c
+++ nanos-lite/src/irq.c
@@ -3,7 +3,7 @@
 static Context* do_event(Event e, Context* c) {
   switch (e.event) {
     case EVENT_YIELD : printf("EVENT_YIELD\n"); break;
-    case EVENT_SYSCALL:printf("syscall\n");     break;
+    case EVENT_SYSCALL:printf("EVENT_SYSCALL\n");     break;
     default: panic("Unhandled event ID = %d", e.event);
   }
 
