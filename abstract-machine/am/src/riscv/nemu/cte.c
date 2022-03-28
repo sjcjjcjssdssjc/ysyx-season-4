e329b49db88beb18f09b0ff2d12e7b70da5c1b89
tracer-ysyx2204 <tracer@ysyx.org>
2022-03-28 06:14:20 -0700

    >  run NEMU ysyx_22040127 shijiacheng Linux ubuntu 5.13.0-35-generic #40~20.04.1-Ubuntu SMP Mon Mar 7 09:18:32 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  06:14:20 up 1 day,  3:25,  1 user,  load average: 0.26, 0.56, 0.51

--- abstract-machine/am/src/riscv/nemu/cte.c
+++ abstract-machine/am/src/riscv/nemu/cte.c
@@ -10,8 +10,9 @@ Context* __am_irq_handle(Context *c) {//handler function(hui diao)
     Event ev = {0};
     switch (c->mcause) {
       case(-1): ev.event = EVENT_YIELD;  break;
-      case(0):  ev.event = EVENT_SYSCALL;break;
-      case(1):  ev.event = EVENT_SYSCALL;break;
+      case(0):  ev.event = EVENT_SYSCALL;break;//sys_exit
+      case(1):  ev.event = EVENT_SYSCALL;break;//sys_yield
+      case(4):  ev.event = EVENT_SYSCALL;break;
       default:  ev.event = EVENT_ERROR;  break;
     }
 
--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -1,5 +1,16 @@
 #include <common.h>
 #include "syscall.h"
+uintptr_t sys_write(uintptr_t a[4]){
+  if(a[1] == 1 || a[1] == 2){
+    char   *buf  = (char *)a[2];
+    size_t count = a[3];
+    for(size_t i = 0;i < count; i++){
+      putch(buf[i]);
+    }
+    return 0;
+  }
+  return 1;
+}
 void do_syscall(Context *c) {
   //printf("do syscall\n");
   uintptr_t a[4];
@@ -7,8 +18,9 @@ void do_syscall(Context *c) {
   a[1] = c->GPR2;//a0 reg
   a[2] = c->GPR3;//a1 reg
   a[3] = c->GPR4;//a2 reg
-  switch (a[0]) {
-    case(SYS_yield): printf("call sys_yield\n"); yield(); c->GPRx = 0; break;//register intptr_t ret asm (GPRx);
+  switch (a[0]) {//ssize_t write(int fd, const void *buf, size_t count);
+    case(SYS_yield): printf("call sys_yield\n"); c->GPRx = sys_write(a); break;//register intptr_t ret asm (GPRx);
+    case(SYS_write): printf("call sys_write\n"); yield(); c->GPRx = 0; break;
     case(SYS_exit):  printf("call sys_exit\n");  halt(a[1]); break;
     default: panic("Unhandled syscall ID = %lx", a[0]);
   }
