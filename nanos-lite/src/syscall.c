67c4f97a167fbb429ba113cd117bc86e4d9de18e
tracer-ysyx2204 <tracer@ysyx.org>
2022-03-28 07:16:13 -0700

    >  run NEMU ysyx_22040127 shijiacheng Linux ubuntu 5.13.0-35-generic #40~20.04.1-Ubuntu SMP Mon Mar 7 09:18:32 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  07:16:13 up 1 day,  4:27,  1 user,  load average: 0.92, 0.54, 0.50

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -1,15 +1,15 @@
 #include <common.h>
 #include "syscall.h"
-int strace = 1;
+int strace = 0;
 uintptr_t sys_write(uintptr_t a[4]){
-  printf("%d %d\n",a[1],a[3]);
+  //printf("%d %d\n",a[1],a[3]);
   if(a[1] == 1 || a[1] == 2){
     char   *buf  = (char *)a[2];
     size_t count = a[3];
     for(size_t i = 0;i < count; i++){
       putch(buf[i]);
     }
-    printf("%d\n",count);
+    //printf("%d\n",count);
     return count;
   }
   return -1;
