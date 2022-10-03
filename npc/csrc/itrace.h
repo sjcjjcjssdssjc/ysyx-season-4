22fa1e12dc5ed928da6c5484a2c18db8edc0fd6d
史佳成 <1339590461@qq.com>
2022-10-02 23:50:32 -0700

    ok

--- npc/csrc/itrace.h
+++ npc/csrc/itrace.h
@@ -8,5 +8,5 @@ void print_surrounding_inst();
 void disassemble(char *str, int size, uint64_t pc, uint8_t *code, int nbyte);
 //void init_disasm(const char *triple);
 void itrace(uint32_t pc, uint32_t inst);
-#define IRINGBUF_SIZE 8 //must be >= 3
+#define IRINGBUF_SIZE 32 //must be >= 3
 #endif
