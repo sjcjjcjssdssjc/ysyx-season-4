4764edf84eb83aad6ecb71515e21c67537785ea9
史佳成 <1339590461@qq.com>
2022-10-03 08:35:54 -0700

    riscv and morty

--- npc/csrc/itrace.c
+++ npc/csrc/itrace.c
@@ -20,21 +20,12 @@ void itrace(uint32_t pc, uint32_t inst){
   p += sprintf(p, "%x: ", pc);
   int ilen = 4;
   int i;
-  p += sprintf(p, " %08x ", inst);//big endian xianshi
-
-  //printf("%s %ld %x %x %d",p,logbuf + sizeof(logbuf) - p - 1,pc,inst,4);
+  p += sprintf(p, " %08x ", inst);//big endian
   disassemble(p, logbuf + sizeof(logbuf) - p,
        pc, (uint8_t *)&inst, 4);
-  //if(pc >= 0x83000000)
-  //printf("%s\n",logbuf);
   
   sprintf(iringbuf[iring_tail], "%s\n",logbuf);
   iring_tail = (iring_tail + 1) % IRINGBUF_SIZE;
-  //printf("%d\n",iring_tail);
-  
-  //print_surrounding_inst();
-  //printf("%lx: %02x %02x %02x %02x %s\n",pc,inst[0],p);
-  //printf("")
   
 }
 #endif
